c1176f027db66955e16c404b0c66256d847003b8
Fyehong <114237411+Fyehong@users.noreply.github.com>
2023-03-10 23:41:14 +0800

    喝汽水问题_3_10

--- execreises/test.c
+++ execreises/test.c
@@ -1,54 +1,24 @@
-#define _CRT_SECURE_NO_WaRNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<assert.h>
-
-
-////模拟strcpy函数的实现
-//
-//char* my_strcpy(char* dest,const char* src)
-//{
-//	/*assert(dest != NULL);
-//	assert(src != NULL);
-//	while (*src != '\0')
-//	{
-//		*dest = *src;
-//		dest++;
-//		src++;
-//	}
-//	*dest = *src;*/
-//
-//	//优化
-//	assert(dest && src);
-//	int* ret = dest;
-//	while (*dest++ = *src++)
-//	{
-//		;
-//	}
-//	return dest;
-//}
-//int main()
-//{
-//	char arr1[] = "hello ait";
-//	char arr2[20] = { 0 };
-//	my_strcpy(arr2, arr1);
-//	printf("%s\n", arr2);
-//	return 0;
-//}
-
-
-//模拟strlen函数
-int my_strlen(const char* arr)
+//喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以多少汽水（编程实现）
+int how_bottle(int money)
 {
-	int i = 0;
-	assert(arr);
-	while (*arr++)
+	int count = money / 1;
+	int ret = 0;
+	int cou = count;
+	while (count >= 2)
 	{
-		i++;
+		ret += count / 2;
+		count = count / 2 + count % 2;
 	}
-	return i;
+	return ret + cou;
 }
+
 int main()
 {
-	char arr[] = "hello bit";
-	printf("%d", my_strlen(arr));
+	int money = 20;
+	int a_steamwater_price = 1;
+	int two_empty_bottle = 1;
+	int count = how_bottle(money);
+	printf("%d\n", count);
 }
